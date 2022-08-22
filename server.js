010d22ed4f4497692e79073e3a642eccfc4b2ea2
AmandaFrisk <amanda.frisk33@gmail.com>
2022-08-22 16:46:17 -0600

    folder strecture set up and port is listening.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app =express()
+const port = 3002
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen (port, ()=> {
+    console.log('listening on port', port)
+    })
+     
