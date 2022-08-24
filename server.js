1dffaee75d0ae27b65bc5a911f74be1f2943b1a0
AmandaFrisk <amanda.frisk33@gmail.com>
2022-08-23 22:00:20 -0600

    connected Atlas

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app =express()
-const port = 3002
+// const port = 3002
 
 
 //Environment Variables (env)
 //require dotenv - config to set up access for env file so we can pull variables from it instead of hardcoding it in
 require('dotenv').config()
 
+// assign env PORT TO variable
+const PORT = process.env.PORT
 
 //IMPORT CONTROLLER
 const habitsController =require('./controllers/habitsController.js')
@@ -17,8 +19,10 @@ const habitsController =require('./controllers/habitsController.js')
 
 //require mongoose
 const mongoose = require('mongoose')
-//              //speak to mongo  ip address for local host:port/db name  - - - ip is for the local host -your machine is using to talk to itself over the network through the port
-mongoose.connect('mongodb://127.0.0.1:27017/habitsDataBase');
+//set variable for accessing process.env.MONGODB_URI
+const mongoURI = process.env.MONGODB_URI
+//  connect - speak to mongo ip address for local host:port/db name  - - - ip is for the local host -your machine is using to talk to itself over the network through the port
+mongoose.connect(mongoURI)
 //connect - open it and then console log
 mongoose.connection.once('open', () =>{
     console.log('connected to mongo')
@@ -36,7 +40,7 @@ app.use('/habits', habitsController)
 
 
 // LISTEN FOR PORT
-app.listen (process.env.PORT, ()=> {
-    console.log(` Server running on port ${process.env.PORT}`)
+app.listen (PORT, ()=> {
+    console.log(` Server running on port ${PORT}`)
     })
      
