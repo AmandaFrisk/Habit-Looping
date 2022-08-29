258bba4063f410394aa9932083efcbc88335c937
AmandaFrisk <amanda.frisk33@gmail.com>
2022-08-29 13:00:24 -0600

    user can sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,70 @@
 //REQUIRE EXPRESS
 const express = require('express')
+
+//REQUIRE BCRYPT
+const bcrypt=require('bcrypt')
+
+//REQUIRE USER MODEL
+const User = require('../models/users')
+
 //SET UP ROUTER
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.send('user controllers work')
+//REGISTER ROUTE 
+router.get('/register', (req, res)=>{
+    res.render('users/register.ejs')
+})
+
+//WHERE FORM IS GOING TO SUBMIT TO CREATE
+router.post('/register', (req,res)=>{
+    //encrypt passwords with bcrypt library 
+    const salt = bcrypt.genSaltSync(10)
+    //generate the hashed password
+   req.body.password = bcrypt.hashSync(req.body.password, salt)
+   console.log(req.body)
+
+    //check if username already exists
+
+    User.findOne({username: req.body.username}, (err, userExists) =>{
+        if(userExists) {
+            res.send('this username is taken')
+        } else{
+            User.create(req.body, (err, createdUser)=>{
+                console.log(createdUser)
+                res.send('user created')
+            })
+        }
+    })
+
+})
+
+//SIGN IN ROUTE
+router.get('/signin', (req, res)=>{
+    res.render('users/signin.ejs')
+})
+
+//allow user to sign in
+router.post('/signin', (req,res) => {
+    //get user with that username
+    User.findOne({username: req.body.username}, (err, foundUser) => {
+        if (foundUser){
+            //compare password using bcrypt's compareSync function
+            const validLogin = bcrypt.compareSync(req.body.password, foundUser.password)
+            //compareSync returns true if they match and false if they don't 
+            //if they passwords match, log them in
+        if (validLogin){
+            //let session know there is a successful login
+            req.session.currentUser = foundUser
+            res.send('User logged in')
+        } else {
+            //if they dont match
+            res.send('Invalid username or password')
+        }
+        } else {
+            //if they don't exist
+            res.send('Invalid username or password')
+        }
+    })
 })
 
 module.exports= router
